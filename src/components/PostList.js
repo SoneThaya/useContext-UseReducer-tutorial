c6b40ae1b95e0ad477a68384cd1c17ae0139d9aa
Sone <sthaya@gmail.com>
2020-06-27 21:23:03 -0700

    created post card component

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import BlogContext from '../context/blogContext';
+import PostCard from './PostCard'
 
 const PostList = () => {
   const blogContext = useContext(BlogContext);
@@ -12,8 +13,33 @@ const PostList = () => {
   console.log(blogPosts)
 
   return (
-    <div>
-      Post List
+    <div className='posts'>
+      <div className="container">
+        <h2>Posts</h2>
+        {
+          !loading ? (
+            <div className='posts-grid-container'>
+              {
+                blogPosts.map((post, i) => {
+                  return (
+                    <PostCard
+                      key={i}
+                      title={post.title}
+                      image={post.image}
+                      author={post.author}
+                      date={post.date}
+                      id={post.id}
+                    />
+                  )
+                })
+              }
+            </div>
+          ) : (
+              <div>Loading...</div>
+          )
+        }
+      </div>
+      
     </div>
   )
 }
